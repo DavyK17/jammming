2cbef9c537011dbae284da524f2dc1b2f7b94c8a
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-03 18:18:47 +0300

    Step 18

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,10 @@
+export class SearchResults extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                <TrackList />
+            </div>
+        );
+    }
+}
