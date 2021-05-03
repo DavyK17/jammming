e94af36fecf66312c8a9019c0446c07e9980fe90
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-03 19:02:17 +0300

    Editing step 32

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,7 +13,7 @@ export class App extends React.Component {
           <div class="App">
               <SearchBar />
               <div class="App-playlist">
-              <SearchResults results={this.state.searchResults} />
+              <SearchResults searchResults={this.state.searchResults} />
               <Playlist />
               </div>
           </div>
