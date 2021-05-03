21d104620ec94e2716392556ef09ad6f96e3902b
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-03 21:24:36 +0300

    Fixing step 34

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,18 +3,14 @@ import { Track } from "../Track/Track";
 import "./TrackList.css";
 
 export class TrackList extends React.Component {
-    renderTracks() {
-        const tracks = this.props.tracks;
-
-        return tracks.map(track => {
-            return <Track track={track} key={track.id} />
-        })
-    }
-
     render() {
         return (
             <div className="TrackList">
-                {this.renderTracks()}
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track} key={track.id} />
+                    })
+                }
             </div>
         );
     }
