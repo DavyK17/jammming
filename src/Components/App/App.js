3bbb52f9421899793400dc122a37955f96f91719
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-03 22:35:24 +0300

    Creating unique objects for playlistTracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,9 +15,9 @@ class App extends React.Component {
       ],
       playlistName: "DVK's Jams",
       playlistTracks: [
-        {name: "name1", artist: "artist1", album: "album1", id: 1},
-        {name: "name2", artist: "artist2", album: "album2", id: 2},
-        {name: "name3", artist: "artist3", album: "album3", id: 3},
+        {name: "name4", artist: "artist4", album: "album4", id: 4},
+        {name: "name5", artist: "artist5", album: "album5", id: 5},
+        {name: "name6", artist: "artist6", album: "album6", id: 6},
       ],
     };
     this.addTrack = this.addTrack.bind(this);
