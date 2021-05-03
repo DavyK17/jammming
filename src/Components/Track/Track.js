4852833441a7cb1309b9269b1a60417818483ece
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-03 20:39:07 +0300

    Refactoring button

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./Track.css";
 
 export class Track extends React.Component {
-    renderAction() {
-        return <button className="Track-action">{this.props.isRemoval ? "+" : "-"}</button>;
-    }
-
     render() {
         return (
             <div className="Track">
@@ -13,7 +9,7 @@ export class Track extends React.Component {
                     <h3>name</h3>
                     <p>artist | album</p>
                 </div>
-                {this.renderAction()}
+                <button className="Track-action">{this.props.isRemoval ? "+" : "-"}</button>
             </div>
         );
     }
