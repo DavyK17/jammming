3f590dca16892cf0c6a066172dc8e6a1940eae0b
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-03 18:14:29 +0300

    Correcting step 11

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 
-function App() {
-  return (
-    <div>
-        <h1>Ja<span class="highlight">mmm</span>ing</h1>
-        <div class="App">
-            <SearchBar />
-            <div class="App-playlist">
-            <SearchResults />
-            <Playlist />
-            </div>
-        </div>
-    </div>
-  );
+export class App extends React.Component {
+  render () {
+    return (
+      <div>
+          <h1>Ja<span class="highlight">mmm</span>ing</h1>
+          <div class="App">
+              <SearchBar />
+              <div class="App-playlist">
+              <SearchResults />
+              <Playlist />
+              </div>
+          </div>
+      </div>
+    );
+  }
 }
-
-export default App;
