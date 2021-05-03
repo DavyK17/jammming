d7ec50b968f81dfcbffa1815042f27f1b0e60fc8
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-03 18:05:50 +0300

    Step 12

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,10 +6,10 @@ function App() {
     <div>
         <h1>Ja<span class="highlight">mmm</span>ing</h1>
         <div class="App">
-            <!-- Add a SearchBar component -->
+            <SearchBar />
             <div class="App-playlist">
-            <!-- Add a SearchResults component -->
-            <!-- Add a Playlist component -->
+            <SearchResults />
+            <Playlist />
             </div>
         </div>
     </div>
