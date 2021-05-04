1df8e986002f2fc455beb4caf2c9c039e4b8fec2
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-04 15:29:04 +0300

    Step 80

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,10 +1,21 @@
 let token;
+
 const Spotify = {
     getAccessToken() {
         if (token) return token;
 
         const tokenMatch = window.location.href.match(/access_token=([^&]*)/);
-        const expiresMatch = window.location.href.match(/expires_in=([^&]*)/);
+        const expiryMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (tokenMatch && expiryMatch) {
+            token = tokenMatch[1];
+            let expiryTime = Number(expiryMatch[1]);
+
+            window.setTimeout(() => token = "", expiryTime * 1000);
+            window.history.pushState("Access Token", null, "/");
+
+            return token;
+        }
     }
 };
 
