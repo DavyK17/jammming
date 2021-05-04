dc8e80963d91e0bc49cb9848db5c4f330134b32d
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-04 13:23:13 +0300

    Step 64

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -47,7 +48,7 @@ class App extends React.Component {
   savePlaylist() {
     let trackURIs = [];
     let playlist = this.state.playlistTracks;
-    
+
     for (let track in playlist) {
       trackURIs.push(track.uri)
     }
@@ -61,7 +62,7 @@ class App extends React.Component {
               <SearchBar />
               <div className="App-playlist">
                 <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-                <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
+                <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
               </div>
           </div>
       </div>
