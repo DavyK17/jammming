e1dba15b022ff08be6e0d788387845c48cb9dbc5
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-31 23:05:45 +0200

    Passing down selectPlaylist() from playlistList

--- src/Components/PlaylistListItem/PlaylistListItem.js
+++ src/Components/PlaylistListItem/PlaylistListItem.js
@@ -2,9 +2,18 @@ import React from "react";
 import "./PlaylistListItem.css";
 
 export class PlaylistListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick() {
+        this.props.selectPlaylist(this.props.id);
+    }
+
     render() {
         return (
-            <div className="Item">
+            <div className="Item" onClick={this.handleClick}>
                 <div className="Item-information">
                     <h3>{this.props.name}</h3>
                 </div>
