2af95ddc4b871f93dbdbdcfcd9e5f0f7a73cb0e4
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-31 23:05:09 +0200

    Refactoring handleNameChange() + dynamic playlist name change

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -8,14 +8,14 @@ export class Playlist extends React.Component {
         this.handleNameChange = this.handleNameChange.bind(this);
     }
 
-    handleNameChange(e) {
-        this.props.onNameChange(e.target.value)
+    handleNameChange({ target }) {
+        this.props.onNameChange(target.value)
     }
 
     render() {
         return(
             <div className="Playlist">
-                <input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
+                <input defaultValue={"New Playlist"} value={this.props.playlistName} onChange={this.handleNameChange} />
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
                 <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
